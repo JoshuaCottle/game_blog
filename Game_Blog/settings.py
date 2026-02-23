e37a64b5e6c464fc19e812095f8bcfec3a4c90b5
Joshua Cottle <Jcottle33@gmail.com>
2026-02-23 16:27:48 +0000

    Set DEBUG=False and add HTTPS security settings for production

--- Game_Blog/settings.py
+++ Game_Blog/settings.py
@@ -18,8 +18,18 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'game-blog-c826415580ce.herokuapp.com']
+
+# HTTPS security settings for production
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # Application definition
 
